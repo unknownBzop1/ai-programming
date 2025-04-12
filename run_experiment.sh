7da7f5468ae6ca6d5e9d860e983ebd8178645228
권도휘 <gwondohwi@gmail.com>
2025-04-12 08:18:51 +0000

    250412-1718 log2

--- run_experiment.sh
+++ run_experiment.sh
@@ -8,7 +8,7 @@ do
     do
         for lr in 0.00003 0.0001 0.0003 0.001 0.003
         do
-            python3 cifar_training.py --model_type $model --batch_size $batch --epochs 40 --lr $lr --log_path ./log2
+            python3 cifar_training.py --model_type $model --batch_size $batch --epochs 20 --lr $lr
         done
         git add .
         git commit -m date +"%y%m%d-%H%M updates"
